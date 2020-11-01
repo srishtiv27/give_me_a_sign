3c6ecdb8719de7bb42403ba25ff5011ca627f98c
SRISHTI VASHISTHA <srishti@SRISHTIs-MacBook-Pro.local>
2020-11-01 23:23:35 +0530

    Added alphabet pictures

--- lib/screens/alphabets_page.dart
+++ lib/screens/alphabets_page.dart
@@ -5,7 +5,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 class AlphabetsPage extends StatelessWidget {
   static String id = 'alphabets_page';
   List<String> background = [
-    'assets/images/background1.png',
+    'assets/images/alphabets/A.png',
   ];
   List<int> buildIndex() {
     List<int> indices = [];
@@ -33,15 +33,18 @@ class AlphabetsPage extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 80.0,
+            ),
             CarouselSlider(
               options: CarouselOptions(
-                height: 800,
+                height: 450,
                 viewportFraction: 0.8,
                 enlargeCenterPage: true,
-                autoPlay: true,
-                autoPlayInterval: Duration(
-                  seconds: 3,
-                ),
+//                autoPlay: true,
+//                autoPlayInterval: Duration(
+//                  seconds: 3,
+//                ),
               ),
               items: buildIndex().map((i) {
                 return Builder(
@@ -54,7 +57,7 @@ class AlphabetsPage extends StatelessWidget {
                       child: FittedBox(
                         fit: BoxFit.fill,
                         child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(30),
                           child: Image.asset(
                             background[i],
                           ),
