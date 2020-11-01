723351f8024bef3650a189bbda20095ba4878f69
SRISHTI VASHISTHA <srishti@SRISHTIs-MacBook-Pro.local>
2020-11-02 00:15:58 +0530

    Updated dimensions of cards

--- lib/screens/alphabets_page.dart
+++ lib/screens/alphabets_page.dart
@@ -5,7 +5,7 @@ import 'package:give_me_a_sign/constants/assets.dart';
 
 class AlphabetsPage extends StatelessWidget {
   static String id = 'alphabets_page';
-  List<String> background = [
+  List<String> alphabets = [
     kA,
     kB,
     kC,
@@ -35,7 +35,7 @@ class AlphabetsPage extends StatelessWidget {
   ];
   List<int> buildIndex() {
     List<int> indices = [];
-    for (int i = 0; i < background.length; i++) {
+    for (int i = 0; i < alphabets.length; i++) {
       indices.add(i);
     }
     return indices;
@@ -46,31 +46,31 @@ class AlphabetsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF272727),
       body: Padding(
-        padding: EdgeInsets.only(left: 20.0, top: 50, right: 20.0),
+        padding: EdgeInsets.only(left: 20.0, top: 70, right: 20.0),
         child: Column(
           children: <Widget>[
             Center(
               child: Text(
                 'Alphabets',
                 style: TextStyle(
-                  fontSize: 35.0,
+                  fontSize: 50.0,
                   fontWeight: FontWeight.bold,
                   color: Color(0xFFE4E3DF),
                 ),
               ),
             ),
             SizedBox(
-              height: 80.0,
+              height: 100.0,
             ),
             CarouselSlider(
               options: CarouselOptions(
-                height: 450,
+                height: 400,
                 viewportFraction: 0.8,
                 enlargeCenterPage: true,
-                autoPlay: true,
-                autoPlayInterval: Duration(
-                  seconds: 3,
-                ),
+//                autoPlay: true,
+//                autoPlayInterval: Duration(
+//                  seconds: 3,
+//                ),
               ),
               items: buildIndex().map((i) {
                 return Builder(
@@ -85,7 +85,7 @@ class AlphabetsPage extends StatelessWidget {
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(30),
                           child: Image.asset(
-                            background[i],
+                            alphabets[i],
                           ),
                         ),
                       ),
