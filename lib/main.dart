4d1a0d93efd76790418a3a57a8dc8ba8b9a45f93
SRISHTI VASHISTHA <srishti@SRISHTIs-MacBook-Pro.local>
2020-11-02 22:17:11 +0530

    Added carousel to numbers page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:give_me_a_sign/screens/alphabets_page.dart';
 import 'package:give_me_a_sign/screens/learn_menu.dart';
 import 'package:give_me_a_sign/screens/menu_page.dart';
+import 'package:give_me_a_sign/screens/numbers_page.dart';
 
 void main() {
   runApp(GiveMeASign());
@@ -12,11 +13,12 @@ class GiveMeASign extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Give Me a Sign',
-      initialRoute: AlphabetsPage.id,
+      initialRoute: NumbersPage.id,
       routes: {
         MenuPage.id: (context) => MenuPage(),
         LearnMenu.id: (context) => LearnMenu(),
         AlphabetsPage.id: (context) => AlphabetsPage(),
+        NumbersPage.id: (context) => NumbersPage(),
       },
     );
   }
