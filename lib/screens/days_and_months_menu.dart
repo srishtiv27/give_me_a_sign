744e5967813794f77932c4440d869b86778c1518
SRISHTI VASHISTHA <srishti@SRISHTIs-MacBook-Pro.local>
2020-11-02 23:07:00 +0530

    Added navigation routes for days and months menu

--- lib/screens/days_and_months_menu.dart
+++ lib/screens/days_and_months_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:give_me_a_sign/screens/months_page.dart';
+import 'package:give_me_a_sign/screens/days_page.dart';
 
 class DaysAndMonthsMenu extends StatelessWidget {
   static String id = 'days_and_months_menu';
@@ -23,58 +25,68 @@ class DaysAndMonthsMenu extends StatelessWidget {
             SizedBox(
               height: 50.0,
             ),
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  child: Image.asset(
-                    'assets/images/days.png',
-                    height: 270,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  bottom: 10.0,
-                  left: 120,
-                  child: Text(
-                    'Days',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Lobster',
-                      color: Color(0xFF3D3D2A),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, DaysPage.id);
+              },
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    child: Image.asset(
+                      'assets/images/days.png',
+                      height: 270,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                )
-              ],
+                  Positioned(
+                    bottom: 10.0,
+                    left: 120,
+                    child: Text(
+                      'Days',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Lobster',
+                        color: Color(0xFF3D3D2A),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             SizedBox(
               height: 40.0,
             ),
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  child: Image.asset(
-                    'assets/images/months.png',
-                    height: 270,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  bottom: 10.0,
-                  left: 110,
-                  child: Text(
-                    'Months',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Lobster',
-                      color: Color(0xFF3D3D2A),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, MonthsPage.id);
+              },
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    child: Image.asset(
+                      'assets/images/months.png',
+                      height: 270,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                )
-              ],
+                  Positioned(
+                    bottom: 10.0,
+                    left: 110,
+                    child: Text(
+                      'Months',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Lobster',
+                        color: Color(0xFF3D3D2A),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
         ),
